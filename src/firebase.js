1b9fe051e019045bb48a5045f946fa9dc8229ea4
Danmoham <dmoham35@gmail.com>
2023-12-08 09:49:50 +0000

    gave private

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzf02LX6cXGkkLj2Qdq7OOfbZniyycKuA",
-  authDomain: "trackapp-81527.firebaseapp.com",
-  projectId: "trackapp-81527",
-  storageBucket: "trackapp-81527.appspot.com",
-  messagingSenderId: "1080792355684",
-  appId: "1:1080792355684:web:14b63c5f0cad013a532071",
-  measurementId: "G-G7H35L6M8K"
+  apiKey: //,
+  authDomain: //,
+  projectId: //,
+  storageBucket: //,
+  messagingSenderId: //,
+  appId: //,
+  measurementId: //
 }; 
 
 // Initialize Firebase
--- src/libs/config.js
+++ src/libs/config.js
@@ -1,2 +1,2 @@
-const APIKEY = "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"
+const APIKEY = //"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"
 export default APIKEY;
